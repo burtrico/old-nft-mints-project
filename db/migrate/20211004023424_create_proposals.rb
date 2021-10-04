4ac61700bc705cf365176f6392908bbae36954ce
Michael Burt <burtrico@gmail.com>
2021-10-03 22:11:09 -0500

    yooo

---
+++ db/migrate/20211004023424_create_proposals.rb
@@ -0,0 +1,17 @@
+class CreateProposals < ActiveRecord::Migration[6.1]
+  def change
+    create_table :proposals do |t|
+      t.token
+      t.string :title
+      t.text :description
+      t.boolean :active
+      t.integer :approve
+      t.integer :deny
+      t.datetime :start_date
+      t.datetime :end_date
+      t.belongs_to :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
